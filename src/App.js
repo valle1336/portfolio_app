0e8172d90c7e862514c090284ccb8409a4203c6f
valle1336 <112933517+valle1336@users.noreply.github.com>
2024-04-06 16:39:42 +0200

    Routing funkar nu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./views/HomePage";
+import TechStack from "./views/TechStack";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <h1>Hejsan</h1>
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/techstack" element={<TechStack />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,22 +4,24 @@ class Navbar extends Component {
   render() {
     return (
       <div className="border-b-2 border-gray-500">
-        <ul className="flex justify-end items-center my-2">
-          <li className="font-bold text-2xl flex justify-start">
+        <ul className="flex items-center my-2">
+          <li className="flex justify-start font-bold text-2xl ml-2">
             Alex Paulsson
           </li>
-          <li className="mx-4">
-            <a href="">Home</a>
-          </li>
-          <li className="mx-4">
-            <a href="">Github</a>
-          </li>
-          <li className="mx-4">
-            <a href="">CV</a>
-          </li>
-          <li className="mx-4">
-            <a href="">Tech stack</a>
-          </li>
+          <div className="flex justify-end">
+            <li className="mx-4">
+              <a href="/">Home</a>
+            </li>
+            <li className="mx-4">
+              <a href="">Github</a>
+            </li>
+            <li className="mx-4">
+              <a href="">CV</a>
+            </li>
+            <li className="mx-4">
+              <a href="/techstack">Tech stack</a>
+            </li>
+          </div>
         </ul>
       </div>
     );
